92613d21c2b7905e0014afb673b9e3bb235abd99
Jayanth <jjay07572@gmail.com>
2024-11-08 12:16:03 +0530

    modified in asstes

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,7 +22,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import VeeRuby from "../assets/company/VeeRuby.jpg"
+import VeeRuby from "./company/VeeRuby.jpg"
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
